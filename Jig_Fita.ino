d23d92a2af45042b0579714429fad3d6fbbcfbe3
periajk <pericles_eng@ajksound.com.br>
2023-08-29 07:28:32 -0300

    first commit

--- Jig_Fita.ino
+++ Jig_Fita.ino
@@ -1,16 +1,42 @@
+/* 
+JIG DE TESTE UNICO - VALORES:
+p/ Jig 1: 
+  if (digitalRead(switchNumLeds) == HIGH) {
+   virtualNumLed = 25;
+
+   VERDE_TOTAL = 2.14;
+   VERMELHO_TOTAL = 2.15;
+   AZUL_TOTAL = 2.14;
+  }
+  else{
+    virtualNumLed = NUM_LEDS;
+
+    VERDE_TOTAL = 4.10;
+    VERMELHO_TOTAL = 4.10;
+    AZUL_TOTAL = 4.10;
+  }
+  ===================
+p/ Jig 2:
+
+*/
+
+
 #include <FastLED.h>
 
-#define DATA_PIN    5 
-#define NUM_LEDS    50
-#define BRIGHTNESS  255
-#define LED_TYPE    WS2811
+#define DATA_PIN 5
+#define NUM_LEDS 50
+#define BRIGHTNESS 255
+#define LED_TYPE WS2811
 #define COLOR_ORDER BGR
-int k =0;
+
+int k = 0;
 const int analogInPin = A0;
 float sensorValue = 0;
 float outputValue = 0;
 int BotaoPin = 2;
 int botao;
+int switchSelector;
+int virtualNumLed;
 float CorrenteFinal = 0;
 
 int LedVerm = 10;
@@ -19,7 +45,9 @@ int LedAz = 12;
 
 int LedPinErro = 8;
 int LedPinSucess = 9;
-int buzzer = 7; //pin 7
+int buzzer = 7;  //pin 7
+int switchNumLeds = 6;
+
 
 float correnteVerd1 = 0;
 float correnteVerm1 = 0;
@@ -37,6 +65,11 @@ float ganhoAmpop = 7.66;
 float V1 = 0;
 float V2 = 0;
 
+float VERDE_TOTAL = 4.10;
+float VERMELHO_TOTAL = 4.10;
+float AZUL_TOTAL = 4.10;
+
+
 int LED_COUNT = 0;
 int error_count = 0;
 
@@ -48,178 +81,177 @@ void setup() {
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
   FastLED.setBrightness(BRIGHTNESS);
   Serial.begin(9600);
-  pinMode(BotaoPin,INPUT_PULLUP);
+  pinMode(BotaoPin, INPUT_PULLUP);
+  pinMode(switchNumLeds, INPUT_PULLUP);
 
-  pinMode (LedVerm, OUTPUT);
-  pinMode (LedVerd, OUTPUT);
-  pinMode (LedAz, OUTPUT);
-  
-  pinMode (LedPinErro, OUTPUT);
+  pinMode(LedVerm, OUTPUT);
+  pinMode(LedVerd, OUTPUT);
+  pinMode(LedAz, OUTPUT);
+
+  pinMode(LedPinErro, OUTPUT);
   pinMode(LedPinSucess, OUTPUT);
   pinMode(buzzer, OUTPUT);
 
+  switchSelector = digitalRead(switchNumLeds);
+
+  virtualNumLed = NUM_LEDS;
 }
 
 
 void loop() {
 
-if (k == 0){
-apagar();   // Para garantir LEDs apagados no início
-digitalWrite(LedVerm, HIGH);
-digitalWrite(LedVerd, HIGH);
-digitalWrite(LedAz, HIGH);
-FastLED.show();
-LED_COUNT = NUM_LEDS;
-k = 1;
-}
+  if (digitalRead(switchNumLeds) == HIGH) {
+   virtualNumLed = 25;
 
-digitalWrite(LedPinSucess, HIGH);
-delay(150);
-digitalWrite(LedPinSucess, LOW);
-delay(150);
-
-botao = digitalRead(BotaoPin);
-if (botao == LOW)
- {
-  error_count = 0;
-  //Serial.print(LED_COUNT);
-  
-  digitalWrite(LedVerd, LOW); 
-  for (LED_COUNT; 0 < LED_COUNT; --LED_COUNT) 
-  {
-     Verde:
-    
-     for(int i = 0; i < LED_COUNT; ++i) {
-          leds[i] = CRGB(255,0,0);
-    }
-     FastLED.show();
-     //Serial.print(LED_COUNT);
-
-
-     if (LED_COUNT == NUM_LEDS) { delay(1000); } //Espera um pouco mais na primeira medição
-     V1 = correnteShunt();
-
-     //Serial.print("\t V1 Verde = ");    //Primeira medição tentando acender todos os LEDs
-     //Serial.println(V1,4);
-     
-    apagar();
-
-    for(int i = 0; i < LED_COUNT - 1; ++i) {
-    leds[i] = CRGB(255,0,0);
-    }
-     FastLED.show();
-     V2 = correnteShunt();   //Segunda medição tentando acender um led a menos
-     //Serial.print("\t V2 Verde = ");
-     //Serial.println(V2,4);
-     //Serial.print("\t Diferença Verde = ");
-     //Serial.println(V1 - V2,4);
-       
-    if (V1 - V2 < 0.01){
-      error();
-      error_count++;
-     goto Fim;
-    }
+   VERDE_TOTAL = 2.67;
+   VERMELHO_TOTAL = 2.67;
+   AZUL_TOTAL = 2.67;
   }
-     digitalWrite(LedVerd, HIGH);
-    
-    
-     Vermelho:
+  else{
+    virtualNumLed = NUM_LEDS;
 
-     digitalWrite(LedVerm, LOW);
-     LED_COUNT = NUM_LEDS;
-     for (LED_COUNT; 0 < LED_COUNT; --LED_COUNT) 
-        {      
-         for(int i = 0; i < LED_COUNT; ++i) {
-              leds[i] = CRGB(0,255,0);
-        }    
-     FastLED.show();
-     if (LED_COUNT == NUM_LEDS) { delay(100); }    
-     V1 = correnteShunt();
-
-     //Serial.print("\t V1 Vermelho = ");    //Primeira medição tentando acender todos os LEDs
-     //Serial.println(V1,4);
-     
-     apagar();
-
-     for(int i = 0; i < LED_COUNT-1; ++i) {
-     leds[i] = CRGB(0,255,0);
-     }
-     FastLED.show();
-     V2 = correnteShunt();
-     //Serial.print("\t V2 Vermelho = ");
-     //Serial.println(V2,4);
-     //Serial.print("\t Diferença Vermelho = ");
-     //Serial.println(V1 - V2,4);
-
-     if (V1 - V2 < 0.01){
-      error();
-      error_count++;
-      goto Fim;
-     }
+    VERDE_TOTAL = 4.35;
+    VERMELHO_TOTAL = 4.35;
+    AZUL_TOTAL = 4.35;
   }
+
+  if (k == 0) {
+    apagar();  // Para garantir LEDs apagados no início
+    digitalWrite(LedVerm, HIGH);
+    digitalWrite(LedVerd, HIGH);
+    digitalWrite(LedAz, HIGH);
+    FastLED.show();
+    LED_COUNT = virtualNumLed;
+    k = 1;
+  }
+
+  digitalWrite(LedPinSucess, HIGH);
+  delay(150);
+  digitalWrite(LedPinSucess, LOW);
+  delay(150);
+
+  botao = digitalRead(BotaoPin);
+  if (botao == LOW) {
+    error_count = 0;
+
+Verde:
+    digitalWrite(LedVerd, LOW);
+
+    for (int i = 0; i < LED_COUNT; ++i) {
+        leds[i] = CRGB(255, 0, 0);
+      }
+
+      FastLED.show();
+
+      delay(300);  //Espera um pouco mais na primeira medição   
+          
+      V1 = correnteShunt();
+      Serial.print("V1 VERDE: ");
+      Serial.print(V1);
+      Serial.print("\n");
+
+      apagar();
+
+
+      if ( (V1 < (VERDE_TOTAL - 0.04)) || (V1 > (VERDE_TOTAL + 0.04))) {
+        Serial.print("V1 VERDE ERRO: ");
+        Serial.print(V1);
+        Serial.print("\n");
+        Serial.print("erro count: ");
+      
+        error();
+        error_count++;
+        goto Fim;
+        apagar();
+      }
+        
+
+    
+    digitalWrite(LedVerd, HIGH);
+
+
+Vermelho:
+
+    digitalWrite(LedVerm, LOW);
+    LED_COUNT = virtualNumLed;
+
+    for (int i = 0; i < LED_COUNT; ++i) {
+        leds[i] = CRGB(0, 255, 0);
+      }
+
+      FastLED.show();
+
+      delay(300);
+      V1 = correnteShunt();
+      Serial.print("V1 VERMELHO: ");
+      Serial.print(V1);
+      Serial.print("\n");
+
+      apagar();
+
+      if ( (V1 < (VERMELHO_TOTAL - 0.04)) || (V1 > (VERMELHO_TOTAL + 0.04))) {
+        Serial.print("V1 VERMELHO ERRO: ");
+        Serial.print(V1);
+        Serial.print("\n");
+        Serial.print("erro count: ");
+
+
+        error();
+        error_count++;
+        goto Fim;
+      }
+    
 
     digitalWrite(LedVerm, HIGH);
-         
-     Azul:
+
+Azul:
 
     digitalWrite(LedAz, LOW);
-    LED_COUNT = NUM_LEDS;
+    LED_COUNT = virtualNumLed;
 
-   for (LED_COUNT; 0 < LED_COUNT; --LED_COUNT) 
-  {  
+    for (int i = 0; i < LED_COUNT; ++i) {
+        leds[i] = CRGB(0, 0, 255);
+      }
 
-    for(int i = 0; i < LED_COUNT; ++i) {
-        leds[i] = CRGB(0,0,255); 
-        }
-   
-    FastLED.show();
-    if (LED_COUNT == NUM_LEDS) { delay(100); }
-    V1 = correnteShunt();
+      FastLED.show();
+      delay(300);
+      V1 = correnteShunt();
+      Serial.print("V1 AZUL: ");
+      Serial.print(V1);
+      Serial.print("\n");
 
-    //Serial.print("\t V1 Azul = ");    //Primeira medição tentando acender todos os LEDs
-    //Serial.println(V1,4);
-     
-    apagar();
+      apagar();
 
-    for(int i = 0; i < LED_COUNT-1; ++i) {
-    leds[i] = CRGB(0,0,255);
-    }
-     FastLED.show();
-     V2 = correnteShunt();
-     //Serial.print("\t V2 Azul = ");
-     //Serial.println(V2,4);
-     //Serial.print("\t Diferença Azul = ");
-     //Serial.println(V1 - V2,4);
+      if ( (V1 < (AZUL_TOTAL - 0.04)) || (V1 > (AZUL_TOTAL + 0.04))) {
+        Serial.print("V1 AZUL ERRO: ");
+        Serial.print(V1);
+        Serial.print("\n");
+        Serial.print("erro count: ");
 
-    if (V1 - V2 < 0.01){
-      error();
-      error_count++;
-      goto Fim;
+        error();
+        error_count++;
+        goto Fim;
+      }
 
-    }
-}
-      if (error_count < 1) { 
+    if (error_count < 1) {
       sucess();
     }
-    
-  Fim:
-  apagar();
-  FastLED.show();
-  k = 0;
-  while(true) {
-    botao = digitalRead(BotaoPin);
-    if (botao == LOW){
-      break;
+
+Fim:
+    apagar();
+    FastLED.show();
+    k = 0;
+    while (true) {
+      botao = digitalRead(BotaoPin);
+      if (botao == LOW) {
+        break;
+      }
     }
   }
- }
 }
 
 
-int error()
-{
-  //Serial.print("\t ERROR ");
-
+int error() {
   digitalWrite(LedPinErro, HIGH);
   delay(400);
   digitalWrite(LedPinErro, LOW);
@@ -244,8 +276,7 @@ int error()
 }
 
 
-int sucess()
-{
+int sucess() {
   //Serial.print("\t GG ");
   digitalWrite(LedAz, HIGH);
   digitalWrite(LedPinSucess, HIGH);
@@ -256,30 +287,25 @@ int sucess()
   digitalWrite(LedPinSucess, HIGH);
   delay(800);
   digitalWrite(LedPinSucess, LOW);
-
-
 }
 
 
-int apagar()
-{
-  for(int i = 0; i < NUM_LEDS; ++i) {
+int apagar() {
+  for (int i = 0; i < virtualNumLed; ++i) {
     leds[i] = CRGB::Black;
-}
-return 1;
+  }
+  return 1;
 }
 
 
-int buzz()
-{
+int buzz() {
   digitalWrite(buzzer, HIGH);
-  delay(200);        
+  delay(200);
   digitalWrite(buzzer, LOW);
 }
 
-float correnteShunt()
-{ 
-    /*
+float correnteShunt() {
+  /*
   int i = 0; 
   
   for(i; i < 5000; ++i)
@@ -293,23 +319,22 @@ float correnteShunt()
 
 
   /////////////////////////////////////
- 
- float soma = 0;
- int i = 0; 
-  for(i; i < 50; ++i){
-      sensorValue = analogRead(analogInPin);
-      outputValue = mapf(sensorValue, 0, 1023, 0, 5.0);
-      delay(1);
-      soma = outputValue + soma;
+
+  float soma = 0;
+  int i = 0;
+  for (i; i < 50; ++i) {
+    sensorValue = analogRead(analogInPin);
+    outputValue = mapf(sensorValue, 0, 1023, 0, 5.0);
+    delay(1);
+    soma = outputValue + soma;
   }
-  soma = soma/i; 
- 
+  soma = soma / i;
+
   return (soma);
 }
 
 
 
-float mapf(float x, float in_min, float in_max, float out_min, float out_max)
-{
+float mapf(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
